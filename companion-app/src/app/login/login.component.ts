af475946aa4fc5e0e61b6df98cffb1027ebb7c6b
kc <chongkarchun@gmail.com>
2019-08-06 10:06:13 -0500

    implemented login page

---
+++ companion-app/src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'companion-login',
+  templateUrl: './login.component.html'
+})
+
+export class LoginComponent implements OnInit {
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+  }
+}
