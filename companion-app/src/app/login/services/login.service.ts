a7ccb905618a2dc75660fe6bc255af35d89fb903
kc <chongkarchun@gmail.com>
2019-08-06 13:18:44 -0500

    added headers

--- companion-app/src/app/login/services/login.service.ts
+++ companion-app/src/app/login/services/login.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {UserInfo} from "../../entities/UserInfo";
 
 @Injectable()
@@ -8,6 +8,12 @@ export class LoginService {
   transactionUrl = 'http://ec2-34-205-166-79.compute-1.amazonaws.com:3389/hunt/newgame';
 
   constructor(private http: HttpClient){}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin' : '*'
+    })
+  };
 
 
   register(id: string, user: UserInfo) {
@@ -19,7 +25,7 @@ export class LoginService {
   }
 
   postUserInfo(user: UserInfo){
-    return this.http.post<UserInfo>(this.transactionUrl, user).subscribe((res) => {
+    return this.http.post<UserInfo>(this.transactionUrl, user, this.httpOptions).subscribe((res) => {
       console.log(res);
     });
   }
