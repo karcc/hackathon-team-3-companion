a809338dee76fa66a39ed0deb1a7207d8864dbd5
kc <chongkarchun@gmail.com>
2019-08-07 10:04:54 -0500

    leaderboards auto refresh every 2 seconds

--- companion-app/src/app/leaderboard/leaderboard.component.ts
+++ companion-app/src/app/leaderboard/leaderboard.component.ts
@@ -13,18 +13,28 @@ export class LeaderboardComponent implements OnInit {
   highscores: any;
   displayedColumns: string[] = ['position', 'name', 'score'];
   datasource: any;
+  interval: any;
 
   constructor(private leaderboardService: LeaderboardService) { }
 
   ngOnInit() {
+    this.getLeaderboardServiceData();
+    this.interval = setInterval(() => {
+      this.getLeaderboardServiceData();
+    }, 2000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  getLeaderboardServiceData(){
     this.leaderboardService.getHighScores().subscribe((res) => {
       console.log(res);
       this.highscores = res;
       this.datasource = res;
       console.log('highscores array: ' + this.highscores);
     });
-
-
   }
 
 }
