31595d2853a117284212afbf80772c740976f544
jbandy98 <jbandy98@gmail.com>
2019-09-10 09:55:27 -0500

    Updating url

--- companion-app/src/app/leaderboard/services/leaderboard.service.ts
+++ companion-app/src/app/leaderboard/services/leaderboard.service.ts
@@ -5,7 +5,7 @@ import {Observable} from "rxjs";
 
 @Injectable()
 export class LeaderboardService {
-  transactionUrl = 'http://ec2-34-205-166-79.compute-1.amazonaws.com:3389/hunt/scores';
+  transactionUrl = 'http://ec2-34-200-238-159.compute-1.amazonaws.com:3389/hunt/scores';
 
   constructor(private http: HttpClient){}
 
--- companion-app/src/app/services/question-service.ts
+++ companion-app/src/app/services/question-service.ts
@@ -8,14 +8,16 @@ import {UserInfo} from "../entities/UserInfo";
 @Injectable()
 export class QuestionService {
 
+  API_URL = 'http://ec2-34-200-238-159.compute-1.amazonaws.com:3389/hunt/';
+
   constructor(private httpClient: HttpClient) { }
 
   public getQuestion(sessionKey: string): Observable<Questions> {
-    return this.httpClient.get<Questions>(API_URL + 'question/' + sessionKey);
+    return this.httpClient.get<Questions>(this.API_URL + 'question/' + sessionKey);
   }
 
   public sendAnswer(sessionKey: string, choice: string): Observable<UserInfo> {
-    return this.httpClient.get<UserInfo>(API_URL + 'answer/' + sessionKey + '/' + choice);
+    return this.httpClient.get<UserInfo>(this.API_URL + 'answer/' + sessionKey + '/' + choice);
   }
 
 }
